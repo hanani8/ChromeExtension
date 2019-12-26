9bc3d3b3568e9d0826387730a56134abfff0db49
Khan047 <58169366+Khan047@users.noreply.github.com>
2019-12-27 00:45:19 +0530

    Add files via upload
    Now dowloads entered text

--- popup.js
+++ popup.js
@@ -15,12 +15,25 @@ document.addEventListener('DOMContentLoaded', function() {
    
   var travel = document.getElementById("tex").value;
   var x = document.createTextNode(travel);
-  
-
   document.body.appendChild(x);
   document.getElementById("tex").value = ""
+      saveTextasFile(travel);
   
 } 
+
+function saveTextasFile(travel){
+  var a = document.createElement('a');
+  var blob = new Blob([ travel ], {type : "text/plain;charset=UTF-8"});
+  a.href = window.URL.createObjectURL(blob);
+  a.download = 'note1.txt';
+  a.style.display = 'none';
+  document.body.appendChild(a);
+  a.click();
+  
+  
+
+}
+
 
 
 
