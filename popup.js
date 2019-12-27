53ab46cecb9ecdeb710d35847d18c1550887099c
Khan047 <58169366+Khan047@users.noreply.github.com>
2019-12-27 20:34:31 +0530

    removed download, Implemented storage api

--- popup.js
+++ popup.js
@@ -4,38 +4,48 @@
 
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("showfiles").addEventListener("click", show);
   document.addEventListener("keydown", function(e){
     if(e.key == 'Enter'){
       save();
-    }
+        }
+   
   });
+  
 });
 
  function save(){
    
   var travel = document.getElementById("tex").value;
-  var x = document.createTextNode(travel);
+  chrome.storage.sync.set({'Note1':travel},function(){
+
+    alert('your note has been saved');
+    var x = document.createTextNode(travel);
   document.body.appendChild(x);
   document.getElementById("tex").value = ""
-      saveTextasFile(travel);
+  });
+  
+   //   saveTextasFile(travel);
   
 } 
+function show(){
 
-function saveTextasFile(travel){
-  var a = document.createElement('a');
-  var blob = new Blob([ travel ], {type : "text/plain;charset=UTF-8"});
-  a.href = window.URL.createObjectURL(blob);
-  a.download = 'note1.txt';
-  a.style.display = 'none';
-  document.body.appendChild(a);
-  a.click();
-  
-  
-
+  chrome.storage.sync.get('Note1',function(data){
+    alert(data.Note1);
+    //need to replace alert with fuctionality
+    // need to use this get and set function in generators or loops
+  })
 }
-
-
-
+// function saveTextasFile(travel){
+//   var a = document.createElement('a');
+//   var blob = new Blob([ travel ], {type : "text/plain;charset=UTF-8"});
+//   a.href = window.URL.createObjectURL(blob);
+//   a.download = 'note1.txt';
+//   a.style.display = 'none';
+//   document.body.appendChild(a);
+//   a.click();
+  
+// }
 
 
 
